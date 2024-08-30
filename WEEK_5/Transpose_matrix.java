8b7ea6be3e211a132171adac3bd4eb726b1a5663
Adnan Rashid <58908269+addu2146@users.noreply.github.com>
2024-08-29 21:33:43 -0700

    Create Transpose_matrix.java

---
+++ WEEK_5/Transpose_matrix.java
@@ -0,0 +1,43 @@
+public class MatrixTranspose {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Compute the transpose of the matrix
+        int[][] transposedMatrix = transposeMatrix(matrix);
+
+        System.out.println("Original matrix:");
+        printMatrix(matrix);
+
+        System.out.println("Transposed matrix:");
+        printMatrix(transposedMatrix);
+    }
+
+    // Function to compute the transpose of a matrix
+    public static int[][] transposeMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposed = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposed;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
