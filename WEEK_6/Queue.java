a09cb98508f7bf812227a876226b2e5e0dea535e
Adnan Rashid <58908269+addu2146@users.noreply.github.com>
2024-09-06 11:07:57 +0530

    Create Queue.java

---
+++ WEEK_6/Queue.java
@@ -0,0 +1,58 @@
+public class Queue {
+    private int SIZE;
+    private int front;
+    private int back;
+    private int[] QueueArray;
+
+    public Queue(int size) {
+        this.SIZE = size;
+        front = -1;
+        back = -1;
+        QueueArray = new int[size];
+    }
+
+    
+    void enque(int x) {
+        if (back == SIZE - 1) {
+            System.out.println("Queue full");
+        } else {
+            if (front == -1) { 
+                front = 0;
+            }
+            QueueArray[++back] = x;
+            System.out.println("Inserted: " + x);
+        }
+    }
+
+    
+    void deque() {
+        if (front == -1) { 
+            System.out.println("Queue Empty");
+        } else {
+            System.out.println("Element removed: " + QueueArray[front]);
+            front++; 
+        }
+    }
+
+   
+    void peek() {
+        if (front == -1) { 
+            System.out.println("Queue Empty");
+        } else {
+            System.out.println("The front element is: " + QueueArray[front]);
+            System.out.println("The back element is: " + QueueArray[back]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(5);
+        queue.deque();
+        queue.peek();
+        queue.enque(10);
+        queue.enque(20);
+        queue.enque(30);
+        queue.peek();
+        queue.deque();
+        queue.peek();
+    }
+}
